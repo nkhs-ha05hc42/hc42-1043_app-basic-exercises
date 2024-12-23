b017152404e0125805a458d0bbe2e02c99647fd7
hc421043 <nkhshc421043@g.neec.ac.jp>
2024-12-23 11:13:35 +0900

    add: 6-1-4

---
+++ 6-1/6-1-4.mjs
@@ -0,0 +1,28 @@
+const a1 = false
+const b1 = false
+const a2 = true
+const b2 = true
+const a3 = true
+const b3 = false
+const a4 = false
+const b4 = true
+
+console.log(a1 && b1)
+console.log(a1 || b1)
+console.log(!a1)
+console.log(!b1)
+
+console.log(a2 && b2)
+console.log(a2 || b2)
+console.log(!a2)
+console.log(!b2)
+
+console.log(a3 && b3)
+console.log(a3 || b3)
+console.log(!a3)
+console.log(!b3)
+
+console.log(a4 && b4)
+console.log(a4 || b4)
+console.log(!a4)
+console.log(!b4)
