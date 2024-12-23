f22b6e788e87d2543220e1e91de7890a231935bb
hc421043 <nkhshc421043@g.neec.ac.jp>
2024-12-23 13:52:52 +0900

    Add: 6-4-6

---
+++ 6-4/6-4-6.mjs
@@ -0,0 +1,19 @@
+const value = "地獄谷"
+
+switch(value){
+    case "地獄谷":
+        console.log("登別")
+        break
+    case "大沼湯":
+        console.log("登別")
+        break
+    case "ウポポイ":
+        console.log("白老")
+        break
+    case "白老牛":
+        console.log("白老")
+        break
+    default:
+        console.log("対象外")
+    
+}
