63fc8638bded170416eb0122b4377e6a2f7061fb
hc421043 <nkhshc421043@g.neec.ac.jp>
2024-12-23 15:20:24 +0900

    Add: 6-6-3

---
+++ 6-6/6-6-3.mjs
@@ -0,0 +1,8 @@
+const map6_6_3 = new Map()
+map6_6_3.set("A", "カレー")
+map6_6_3.set("B", "定食")
+
+console.log(map6_6_3.has("A"))
+console.log(map6_6_3.get("A"))
+console.log(map6_6_3.has("B"))
+console.log(map6_6_3.get("B"))
