0365bc9b302da3b0e54ec4de3129391cf24cea17
hc421043 <nkhshc421043@g.neec.ac.jp>
2024-12-23 13:52:35 +0900

    Add: 6-4-3

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,11 @@
+const value = 65
+
+if(value >= 90){
+    console.log("Excellent")
+}else if(value >= 80){
+    console.log("Good")
+}else if(value >= 60){
+    console.log("Normal")
+}else{
+    console.log("Red")
+}
