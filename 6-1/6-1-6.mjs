d33b7ab36403f59491a0eafd9ec78f6435a57ed5
hc421043 <nkhshc421043@g.neec.ac.jp>
2024-12-23 11:13:47 +0900

    add: 6-1-6

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,13 @@
+const a = 10
+const b = 5
+const c = 15
+
+console.log(a > b)
+console.log(a >= b)
+console.log(a < b)
+console.log(a <= b)
+
+console.log(a > c)
+console.log(a >= c)
+console.log(a < c)
+console.log(a <= c)
