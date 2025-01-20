e4bbb5cc323aec248661973977a8f52d9eb14f18
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-01-20 12:09:41 +0900

    add: 7-1-4

---
+++ 7-1/7-1-4.mjs
@@ -0,0 +1,11 @@
+const function714 = (dat1) => {
+    
+    return dat1.value
+}
+
+const dat1 = { 
+    name: "リンゴ", 
+    value: "apple" 
+}
+const result = function714(dat1)
+console.log("function714", result)
