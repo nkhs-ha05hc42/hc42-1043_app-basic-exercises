46ed7fca109e178118fdf52e41747d48f7df05ab
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-01-20 13:01:14 +0900

    add: 7-2-3

---
+++ 7-2/7-2-3.mjs
@@ -0,0 +1,6 @@
+import {function723_1, function723_2} from "/workspace/nkhs-node-ex-basic/7-2/7-2-3_export.mjs"
+
+const result = function723_1(3, 5)
+console.log("function723_1", result)
+const result2 = function723_2(3, 5)
+console.log("function723_2", result2)
---
+++ 7-2/7-2-3_export.mjs
@@ -0,0 +1,7 @@
+export const function723_1 = (a, b) => { 
+    return a * b 
+} 
+
+export const function723_2 = (a, b) => {
+    return a / b
+}
