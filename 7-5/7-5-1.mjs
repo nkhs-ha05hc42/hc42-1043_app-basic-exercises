b01ecc30d2000e263731bf17b92baa650203a0d0
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-01-20 14:50:29 +0900

    add: 7-5-1

---
+++ 7-5/7-5-1.mjs
@@ -0,0 +1,21 @@
+const function751SetTimeout = (timeout) => 
+    new Promise((resolve, reject) => { 
+      setTimeout(() => {  
+        resolve() 
+      }, timeout) 
+    }) 
+   
+const function751 = () => { 
+    function751SetTimeout(10000) 
+      .then(() => { 
+        console.log("★", new Date()) 
+        return function751SetTimeout(10000) 
+      }) 
+      .then(() => { 
+        console.log("◆", new Date()) 
+      }) 
+} 
+   
+console.log("start", new Date()) 
+function751() 
+console.log("end", new Date()) 
