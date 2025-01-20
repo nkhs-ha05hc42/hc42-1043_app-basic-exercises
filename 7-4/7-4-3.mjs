9fa193ff43b5f8b8116c6111c847ee56d88a2621
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-01-20 16:09:23 +0900

    add: 7-4-3 ver.2

--- 7-4/7-4-3.mjs
+++ 7-4/7-4-3.mjs
@@ -3,8 +3,8 @@ setTimeout(() => {
     console.log("★", new Date()) 
     setTimeout(() => {
         console.log("◆", new Date())
-        setTimeout(() => {
-            console.log("end", new Date()) 
-        }, 2000)
     }, 3000)
+    setTimeout(() => {
+        console.log("end", new Date()) 
+    }, 5000)
 }, 2000) 
