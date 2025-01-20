6373df7422d6e11f2e40a09a92470fc5c9afa5bc
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-01-20 16:09:55 +0900

    add: 7-5-3 ver.2

--- 7-5/7-5-3.mjs
+++ 7-5/7-5-3.mjs
@@ -1,25 +1,22 @@
-const function753SetTimeout = (timeout) => 
+const function752SetTimeout = (timeout) => 
     new Promise((resolve, reject) => { 
       setTimeout(() => {  
         resolve() 
       }, timeout) 
     }) 
    
-const function753 = () => { 
-    function753SetTimeout(2000) 
+const function752 = () => { 
+    function752SetTimeout(2000) 
       .then(() => { 
         console.log("★", new Date()) 
-        return function753SetTimeout(3000) 
-      }) 
-      .then(() => { 
-        console.log("◆", new Date()) 
-        return function753SetTimeout(5000)
-      }) 
-      .then(() => {
-        console.log("end", new Date()) 
+         function752SetTimeout(3000).then(() => {
+            console.log("◆", new Date()) 
+         })
+         function752SetTimeout(5000).then(() => {
+            console.log("end", new Date())
+         })
       })
-
 }
    
 console.log("start", new Date()) 
-function753() 
+function752() 
