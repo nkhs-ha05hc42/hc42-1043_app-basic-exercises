28484f27a54223249822614e9e0d563574be6444
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-01-27 12:34:45 +0900

    add: q8_1_3

--- server/api/8-1/q8_1Controller.mjs
+++ server/api/8-1/q8_1Controller.mjs
@@ -12,7 +12,13 @@ const post8_1_2 = (req, res) => {
     res.send(JSON.stringify({ status: "OK", postcode: "059-0012"}))
 }
 
+const put8_1_3 = (req, res) => {
+    console.log(req.body)
+    res.send(JSON.stringify({ status: "OK", postcode: "059-0012"}))
+}
+
 export const q8_1Controller = {
     get8_1_1,
     post8_1_2,
+    put8_1_3,
 }
--- server/routers.mjs
+++ server/routers.mjs
@@ -15,6 +15,7 @@ routers.get("/api/sample1/:id", sample1Controller.getPathParamSample1)
 
 routers.get("/api/8-1", q8_1Controller.get8_1_1)
 routers.post("/api/8-1", q8_1Controller.post8_1_2)
+routers.put("/api/8-1", q8_1Controller.put8_1_3)
 
 // client配下は、ブラウザサイドで実行されるファイル（HTML/JS/CSS/画像など）を配置する
 routers.use(express.static(`${path.resolve()}/client`))
