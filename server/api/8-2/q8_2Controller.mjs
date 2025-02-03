c4ca4fe1ea63e421c6b23d4b3198cf2e2807da86
hc421043 <nkhshc421043@g.neec.ac.jp>
2025-02-03 11:21:30 +0900

    add: q8_2_2

--- server/api/8-2/q8_2Controller.mjs
+++ server/api/8-2/q8_2Controller.mjs
@@ -4,8 +4,7 @@ const get8_2_1 = (req, res) => {
         "code": "HC42-9821",
         "name": "情報太郎"
     }
-    if(req.query.code != data.code){
-        
+    if(req.query.code !== data.code){
         res.send(JSON.stringify({ status: "error", code: req.query.code, cause: "not found code!"}))
         return
     }
@@ -13,6 +12,20 @@ const get8_2_1 = (req, res) => {
     res.send(JSON.stringify({ code: req.query.code, data: "情報太郎"}))
 }
 
+const post8_2_2 = (req, res) => {
+    const data = {
+        "code": "HC42-9821",
+    }
+    if(req.body.code === data.code){ 
+        res.send(JSON.stringify({ status: "error", code: req.body.code, cause: "code already exists!"}))
+        return
+    }
+
+    console.log(req.body)
+    res.send(JSON.stringify({ status: "OK", postcode: "059-0012"}))
+}
+
 export const q8_2Controller = {
     get8_2_1,
+    post8_2_2,
 }
--- server/routers.mjs
+++ server/routers.mjs
@@ -20,6 +20,7 @@ routers.put("/api/8-1", q8_1Controller.put8_1_3)
 routers.delete("/api/8-1", q8_1Controller.delete8_1_4)
 
 routers.get("/api/8-2", q8_2Controller.get8_2_1)
+routers.post("/api/8-2", q8_2Controller.post8_2_2)
 
 // client配下は、ブラウザサイドで実行されるファイル（HTML/JS/CSS/画像など）を配置する
 routers.use(express.static(`${path.resolve()}/client`))
